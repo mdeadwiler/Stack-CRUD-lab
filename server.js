3edf80e095ae43f49ddf29ef3a0aa205a2b0dc34
MDeadwiler <mdeadwiler@email.com>
2024-09-27 13:09:41 -0400

    cypto-techTalk

--- server.js
+++ server.js
@@ -1,3 +1,44 @@
+// We begin by loading Express
+const dotenv = require("dotenv"); // This is required package
+dotenv.config(); // This loads the environment variables .env file
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // This is a required package
+
+
+
 const app = express();
+
+
+// Connect to MongoDB using the connection string in the .env file
+mongoose.connect(process.env.MONGODB_URI);
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+// Import the CryptoTalk model
+const CryptoTalk = require("./models/cryptoTalk.js");
+
+// GET |index.ejs |server.js
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+
+
+
+
+
+
+
+// server.js
+
+// GET /blog/new
+app.get("/blogs/new", (req, res) => {
+    res.render("blogNew.ejs");
+  });
+  
+
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
