be33a9ef8af71b6fd06bce4673b1e54df8ea0c08
MDeadwiler <mdeadwiler@email.com>
2024-09-29 14:17:02 -0400

    stack-crud-lab

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
-// We begin by loading Express
 const dotenv = require("dotenv"); // This is required package
 dotenv.config(); // This loads the environment variables .env file
 const express = require("express");
 const mongoose = require("mongoose"); // This is a required package
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+// Import the CryptoTalk model
+const CryptoTalk = require("./models/cryptoTalk.js");
+
 // Connect to MongoDB using the connection string in the .env file
 mongoose.connect(process.env.MONGODB_URI);
 // log connection status to terminal on start
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-// Import the CryptoTalk model
-const CryptoTalk = require("./models/cryptoTalk.js");
+app.use(express.urlencoded({ extended: false }));
 
-
-
-// GET |index.ejs |server.js
+//Routes|Golden 7
+// GET |index.ejs |Home
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
-
-// GET.Render /blog/new
-app.get("/pages/new", (req, res) => {
-    res.render("pages/new.ejs");
-  });
-  
-
-
-
-// POST /New Blog
+// GET.Render |blog/new
+app.get("/pages/new", async (req, res) => {
+  res.render("pages/new.ejs");
+});
+// POST |New Blog
 app.post("/pages", async (req, res) => {
-  await CryptoTalk.create(req.body);
+//  await CryptoTalk.create(req.body);
+console.log(req.body)
   res.redirect("pages/new");
 });
 
